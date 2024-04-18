29439c942babdb9578c0119afece4ee3d817ccef
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-18 17:49:20 +0200

    add randChoice func

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const genders = ["M", "F"];
 const maleNames = [Adam, Bartosz, Cezary, Dariusz, Ernest];
 const femaleNames = [Aleksandra, Barbara, Celina, Danuta, Ewa];
 const lastNames = [Antczak, Braszak, Chyra, Dudek, Eward];
+
+const randChoice = (arr) => {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+};
