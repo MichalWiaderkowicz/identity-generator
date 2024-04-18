234c344ed3c58b96ce344ca8a18dee04d759f677
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-04-18 17:39:20 +0200

    add app.js with import fs and  tables ( gender, malenames, femalenames, lastnames)

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+const genders = ["M", "F"];
+const maleNames = [Adam, Bartosz, Cezary, Dariusz, Ernest];
+const femaleNames = [Aleksandra, Barbara, Celina, Danuta, Ewa];
+const lastNames = [Antczak, Braszak, Chyra, Dudek, Eward];
